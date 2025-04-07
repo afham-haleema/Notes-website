542e388ffb42588b8b62e576a042dc32cdb70103
afham-haleema <afhamhaleema@gmail.com>
2025-04-07 21:46:43 +0300

    committed

--- server/server.js
+++ server/server.js
@@ -7,17 +7,29 @@ require('dotenv').config()
 const app=express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+app.use(cors({
+    origin: "*", // Allow all origins or specify specific domains
+  }));
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-    app.listen(5000,()=>{
-        console.log('listening..')
-    })
-}).catch(err=>{
-    console.log("error connecting")
-    console.log(err);
-})
+// mongoose.connect(process.env.MONGO_URI).then(()=>{
+//     app.listen(5000,()=>{
+//         console.log('listening..')
+//     })
+// }).catch(err=>{
+//     console.log("error connecting")
+//     console.log(err);
+// })
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // Export the Express app as a Vercel handler
+    module.exports = app;
+  })
+  .catch(err => {
+    console.log("Error connecting to MongoDB");
+    console.log(err);
+  });
