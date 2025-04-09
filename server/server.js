397fbba296729edbfccfb7dd7745988fd85cc359
afham-haleema <afhamhaleema@gmail.com>
2025-04-09 20:33:34 +0300

    added

--- server/server.js
+++ server/server.js
@@ -4,10 +4,8 @@ const cors=require('cors')
 require('dotenv').config()
 
 const app=express()
-
-app.use(express.json())
 app.use(cors())
-
+app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
