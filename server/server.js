5742fefaf92d8dccbf6ef9d1aa0a9b0182e191e0
afham-haleema <afhamhaleema@gmail.com>
2025-04-09 20:28:32 +0300

    added

--- server/server.js
+++ server/server.js
@@ -2,21 +2,17 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 require('dotenv').config()
-MONGO_URI="mongodb+srv://afham:afham1234@nodetuts.opiyiwf.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts"
-
 
 const app=express()
 
 app.use(express.json())
-// app.use(cors())
-app.use(cors({
-    origin: "*", // Allow all origins or specify specific domains
-  }));
+app.use(cors())
+
 
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
-mongoose.connect(MONGO_URI).then(()=>{
+mongoose.connect(process.env.MONGO_URI).then(()=>{
     app.listen(5000,()=>{
         console.log('listening..')
     })
